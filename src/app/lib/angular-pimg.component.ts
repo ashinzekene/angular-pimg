c536a27e1648b5bfb86c231bfc8960427f09a7a5
Ashinzekene <ashinzekene@gmail.com>
2018-05-26 20:34:38 +0100

    fixed TSLINT, rewrite of test component

--- src/app/lib/angular-pimg.component.ts
+++ src/app/lib/angular-pimg.component.ts
@@ -9,136 +9,125 @@ import { AngularPimgOptions } from './config-options';
   styles: []
 })
 export class AngularPimgComponent implements OnChanges {
-  @Input() dataSaver: any
-  @Input() src: string
-  @Input() fetchOnDemand: boolean
-  @Input() placeholder: string
-  @Input() placeholderClassName: string
-  @Input() className: string
-  @Input() style: any = { display: 'block' }
-  @Output() onFetched: EventEmitter<null> = new EventEmitter()
-  @Output() onError: EventEmitter<any> = new EventEmitter()
-  wrapperClassName: string = ''
-  buttonClassName: string = ''
-  blob: SafeUrl = ''
-  delayed: boolean
-  loading: boolean
-  classes: string
-  private isIntersecting: boolean = false
+  @Input() dataSaver: any;
+  @Input() src: string;
+  @Input() fetchOnDemand: boolean;
+  @Input() placeholder: string;
+  @Input() placeholderClassName: string;
+  @Input() className: string;
+  @Input() style: any = { display: 'block' };
+  @Output() fetched: EventEmitter<null> = new EventEmitter();
+  @Output() error: EventEmitter<any> = new EventEmitter();
+  wrapperClassName = '';
+  buttonClassName = '';
+  blob: SafeUrl = '';
+  delayed: boolean;
+  loading: boolean;
+  classes: string;
+  private isIntersecting = false;
 
   constructor(private el: ElementRef, private domSanitizer: DomSanitizer, private options: AngularPimgService) {
-    this.insertInput()
-    this.setUp()
+    this.insertInput();
+    this.setUp();
   }
 
   setFetchOnDemand() {
-    let observer = new IntersectionObserver(entries => {
-      let image = entries[0]
+    const observer = new IntersectionObserver(entries => {
+      const image = entries[0];
       if (image.isIntersecting && !this.isIntersecting) {
-        this.fetchImage()
-        this.isIntersecting = true
-        console.log("Called by intersection API")
-        this.delayed = false
-        observer.disconnect()
+        this.fetchImage();
+        this.isIntersecting = true;
+        console.log('Called by intersection API');
+        this.delayed = false;
+        observer.disconnect();
       }
-    })
-    observer.observe(this.el.nativeElement)
+    });
+    observer.observe(this.el.nativeElement);
   }
 
   fetchImage() {
     fetch(this.src)
       .then(res => res.blob())
       .then(res => {
-        console.log(res)
-        this.blob = this.domSanitizer.bypassSecurityTrustUrl(URL.createObjectURL(res))
-        this.loading = false
-        this.isIntersecting = false
-        this.delayed = false
-        this.onFetched.emit()
+        console.log(res);
+        this.blob = this.domSanitizer.bypassSecurityTrustUrl(URL.createObjectURL(res));
+        this.loading = false;
+        this.isIntersecting = false;
+        this.delayed = false;
+        this.fetched.emit();
       })
       .catch(err => {
-        this.onError.emit(err)
-      })
+        this.error.emit(err);
+      });
   }
 
   ngOnChanges(changes: SimpleChanges) {
-    let newValues: any = {}
-    changes.dataSaver ? newValues.dataSaver = changes.dataSaver.currentValue : null
-    changes.src ? newValues.src = changes.src.currentValue : null
-    changes.fetchOnDemand ? newValues.fetchOnDemand = changes.fetchOnDemand.currentValue : null
-    changes.placeholder ? newValues.placeholder = changes.placeholder.currentValue : null
-    changes.placeholderClassName ? newValues.placeholderClassName = changes.placeholderClassName.currentValue : null
-    changes.class ? newValues.class = changes.class.currentValue : null
-    changes.style ? newValues.style = changes.style.currentValue : null
-    this.insertInput(newValues)
-    this.setUp()
+    const newValues: any = {};
+    const props = ['src', 'fetchOnDemand', 'placeholder', 'placeholderClassName', 'className', 'style'];
+    for (const prop in changes) {
+      if (props.includes(prop) && !this.isUndefined(changes[prop].currentValue)) {
+        console.log(`${prop} is not undefined. It has changed`);
+        this[prop] = changes[prop].currentValue;
+      } else {
+        if (prop === 'dataSaver' && this.isObject(changes[prop])) {
+          this.buttonClassName = changes[prop].currentValue.buttonClassName;
+          this.placeholderClassName = changes[prop].currentValue.placeholderClassName;
+        }
+      }
+    }
+    this.setUp();
+    // changes.dataSaver ? newValues.dataSaver = changes.dataSaver.currentValue : null;
+    // changes.src ? newValues.src = changes.src.currentValue : null;
+    // changes.fetchOnDemand ? newValues.fetchOnDemand = changes.fetchOnDemand.currentValue : null;
+    // changes.placeholder ? newValues.placeholder = changes.placeholder.currentValue : null;
+    // changes.placeholderClassName ? newValues.placeholderClassName = changes.placeholderClassName.currentValue : null;
+    // changes.class ? newValues.class = changes.class.currentValue : null;
+    // changes.style ? newValues.style = changes.style.currentValue : null;
   }
 
-  insertInput(values?) {
-    if (values) {
-      // Called by simple changes; Set the default configuration options if option is not present
-      if (this.isUndefined(values.fetchOnDemand)) {
-        values.fetchOnDemand = this.options.fetchOnDemand
-      }
-      if (this.isUndefined(values.placeholderClassName)) {
-        values.placeholderClassName = this.options.placeholderClassName
-      }
-      if (this.isUndefined(values.className)) {
-        values.className = this.options.className
-      }
-      if (this.isUndefined(values.dataSaver)) {
-        // values.dataSaver = values.options.dataSaver
-      }
-      if (this.isObject(values.dataSaver)) {
-        // set buttonClassName and wrapperClassName
-        this.buttonClassName = this.dataSaver.buttonClassName
-        this.wrapperClassName = this.dataSaver.wrapperClassName
-      } else {
-        this.buttonClassName = this.options.buttonClassName
-        this.wrapperClassName = this.options.wrapperClassName
-      }
+  insertInput() {
+    // Called on initialization; Set the default configuration options if option is not present
+    console.log('Config options', this.options);
+    if (this.isUndefined(this.fetchOnDemand)) {
+      this.fetchOnDemand = this.options.fetchOnDemand;
+    }
+    if (this.isUndefined(this.placeholderClassName)) {
+      this.placeholderClassName = this.options.placeholderClassName;
+    }
+    if (this.isUndefined(this.dataSaver)) {
+      this.dataSaver = this.options.dataSaver;
+    }
+    if (this.isUndefined(this.className)) {
+      this.className = this.options.className;
+    }
+    if (this.isObject(this.dataSaver)) {
+      // set buttonClassName and wrapperClassName
+      this.buttonClassName = this.dataSaver.buttonClassName;
+      this.wrapperClassName = this.dataSaver.wrapperClassName;
     } else {
-      // Called on initialization; Set the default configuration options if option is not present
-      if (this.isUndefined(this.fetchOnDemand)) {
-        this.fetchOnDemand = this.options.fetchOnDemand
-      }
-      if (this.isUndefined(this.placeholderClassName)) {
-        this.placeholderClassName = this.options.placeholderClassName
-      }
-      if (this.isUndefined(this.dataSaver)) {
-        this.dataSaver = this.options.dataSaver
-      }
-      if (this.isUndefined(this.className)) {
-        this.className = this.options.className
-      }
-      if (this.isObject(this.dataSaver)) {
-        // set buttonClassName and wrapperClassName
-        this.buttonClassName = this.dataSaver.buttonClassName
-        this.wrapperClassName = this.dataSaver.wrapperClassName
-      } else {
-        this.buttonClassName = this.options.buttonClassName
-        this.wrapperClassName = this.options.wrapperClassName
-      }
+      this.buttonClassName = this.options.buttonClassName;
+      this.wrapperClassName = this.options.wrapperClassName;
     }
   }
 
   setUp() {
     if (this.dataSaver) {
-      this.delayed = true
+      this.delayed = true;
     } else if (this.fetchOnDemand) {
-      this.loading = true
-      this.setFetchOnDemand()
+      this.loading = true;
+      this.setFetchOnDemand();
     } else {
-      this.fetchImage()
-      console.log("Fetching image, due to config", this.dataSaver)
+      this.fetchImage();
+      console.log('Fetching image, due to config', this.dataSaver);
     }
     if (this.src && this.src.includes('cloudinary')) {
       this.placeholder =
-        this.placeholder || this.src.replace('/upload/', '/upload/c_thumb,w_30/')
+        this.placeholder || this.src.replace('/upload/', '/upload/c_thumb,w_30/');
     }
-    this.classes = `${this.className} ${this.placeholderClassName}`
+    this.classes = `${this.className} ${this.placeholderClassName}`;
   }
-  isObject = x => typeof x === 'object'
-  isUndefined = x => typeof x === 'undefined'
+
+  isObject = x => typeof x === 'object';
+  isUndefined = x => typeof x === 'undefined';
 
 }
--- src/app/lib/angular-pimg.service.ts
+++ src/app/lib/angular-pimg.service.ts
@@ -6,51 +6,55 @@ import { AngularPimgOptions } from './config-options';
   providedIn: 'root'
 })
 export class AngularPimgService {
-  private _className: string
-  private _placeholderClassName: string = 'pimg__placeholder';
-  private _fetchOnDemand: boolean = false;
-  private _dataSaver: boolean
-  private _buttonClassName: string = 'pimg_btn'
-  private _wrapperClassName: string = 'pimg_wrapper'
+  private _className: string;
+  private _placeholderClassName = 'pimg__placeholder';
+  private _fetchOnDemand = false;
+  private _dataSaver: boolean;
+  private _buttonClassName = 'pimg_btn';
+  private _wrapperClassName = 'pimg_wrapper';
 
   constructor(@Optional() @Inject('Options') options: Partial<AngularPimgOptions>) {
-    if (options) this.setConfig(options)
+    if (options) {
+      this.setConfig(options);
+    }
   }
 
   get wrapperClassName(): string {
-    return this._wrapperClassName
+    return this._wrapperClassName;
   }
 
   get buttonClassName(): string {
-    return this._buttonClassName
+    return this._buttonClassName;
   }
 
   get className(): string {
-    return this._className
+    return this._className;
   }
 
   get placeholderClassName(): string {
-    return this._placeholderClassName
+    return this._placeholderClassName;
   }
 
   get dataSaver(): boolean {
-    return this._dataSaver
+    return this._dataSaver;
   }
 
   get fetchOnDemand(): boolean {
-    return this._fetchOnDemand
+    return this._fetchOnDemand;
   }
 
   setConfig({ className, dataSaver, fetchOnDemand, placeholderClassName }: Partial<AngularPimgOptions>) {
-    this._className = typeof className !== "undefined" ? className : this.className
-    this._fetchOnDemand = typeof fetchOnDemand !== "undefined" ? fetchOnDemand : this.fetchOnDemand
-    this._placeholderClassName = typeof placeholderClassName !== "undefined" ? placeholderClassName : this.placeholderClassName
-    if (typeof dataSaver === 'undefined') return
+    this._className = typeof className !== 'undefined' ? className : this.className;
+    this._fetchOnDemand = typeof fetchOnDemand !== 'undefined' ? fetchOnDemand : this.fetchOnDemand;
+    this._placeholderClassName = typeof placeholderClassName !== 'undefined' ? placeholderClassName : this.placeholderClassName;
+    if (typeof dataSaver === 'undefined') {
+      return;
+    }
     if (typeof dataSaver === 'object') {
-      this._buttonClassName = dataSaver.buttonClassName
-      this._wrapperClassName = dataSaver.wrapperClassName
+      this._buttonClassName = dataSaver.buttonClassName;
+      this._wrapperClassName = dataSaver.wrapperClassName;
     } else {
-      this._dataSaver = true // dataSaver is true
+      this._dataSaver = true; // dataSaver is true
     }
   }
 
